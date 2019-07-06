8d5ecfa04a4af25029792bd8a6820544b3448a7e
alexandrercosta01@gmail.com <alexandrercosta01@gmail.com>
2019-07-06 14:10:58 -0300

    primeiro commit

--- ConsultaCEP/Controllers/CepController.cs
+++ ConsultaCEP/Controllers/CepController.cs
@@ -29,7 +29,7 @@ namespace ConsultaCEP.Controllers
         public IActionResult ConsultarCep(CEP Ceps)
         {
             WebClient client = new WebClient();
-            string json = client.DownloadString("https://viacep.com.br/ws/"+Ceps.Cep+"/json/");
+            string json = client.DownloadString("https://viacep.com.br/ws/"+Ceps.Cep+"/json/"); 
             CEP cep = JsonConvert.DeserializeObject<CEP>(json);
             _context.Ceps.Add(cep);
             _context.SaveChanges();

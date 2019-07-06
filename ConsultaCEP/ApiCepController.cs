bc7ae0b74658867e5e4aa46f4c6c47a45f323e60
alexandrercosta01@gmail.com <alexandrercosta01@gmail.com>
2019-07-06 15:26:29 -0300

    Ultimo Commit

---
+++ ConsultaCEP/ApiCepController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ConsultaCEP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AppCep
+{
+    [Route("api/Endereco")]
+    public class RestCepController : Controller
+    {
+
+        private readonly Context _context;
+
+        public RestCepController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Enderecos")]
+        public IEnumerable<CEP> GetAll()
+        {
+            List<CEP> ceps = _context.Ceps.ToList();
+            return ceps;
+        }
+
+
+        [HttpGet("Enderecos/{cep}")]
+        public IEnumerable<CEP> GetPorCep(string cep)
+        {
+            var ceps = _context.Ceps.Where(s => s.Cep == cep);
+            return ceps;
+        }
+
+        [HttpGet("EnderecosPorEstado/{uf}")]
+        public IEnumerable<CEP> GetPorEstado(string uf)
+        {
+            List<CEP> ceps = _context.Ceps.Where(s => s.Uf == uf).ToList();
+            return ceps;
+        }
+    }
+}
